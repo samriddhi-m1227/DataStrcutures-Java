a413c2987451d58fc751ff71d122ca20231f1b15
Samriddhi Matharu <samriddhimatharu1@gmail.com>
2024-06-06 13:48:42 -0700

    Delete Driver.java

--- Driver.java
+++
@@ -1,9 +0,0 @@
-public class Driver {
-  public static void main(String[] args) {
-     Rectangle rect = new Rectangle(10, 20);
-     Circle circle = new Circle(10);
-     Triangle triangle = new Triangle(10, 20, 30);
-     // NOTE: The actual driver program will run tests on the above objects
-	System.out.println("foobar");
-  }
-}
